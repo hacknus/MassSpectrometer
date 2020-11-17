bd7ada932fa58df58c48d1e0e9bd890f1406b128
donathess <62433205+donathess@users.noreply.github.com>
2020-11-18 00:21:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import os
 from read_data import hist
 
-
 def process_data(filename, baseline_filename):
     path = "DataAvg/" + filename
     baseline_path = "DataAvg/" + baseline_filename
@@ -12,9 +11,9 @@ def process_data(filename, baseline_filename):
     df_noise = pd.read_csv(baseline_path)
     # subtract noise
     df["p"] = df["p"] - df_noise["p"]
-    df["err"] = np.sqrt(df["err"] ** 2 + df_noise["err"] ** 2)
+    df["err"] = np.sqrt(df["err"]**2 + df_noise["err"]**2)
     hist(df, filename, combine=1, plot=True)
-
+    
 
 if __name__ == '__main__':
     process_data("Xenon.csv", "xenonbaseline.csv")
