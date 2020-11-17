e40fef0bcac763a08e99d63c8800e95e9889fb46
donathess <62433205+donathess@users.noreply.github.com>
2020-11-18 00:21:36 +0100

    Create edelgas_analysis.py

---
+++ edelgas_analysis.py
@@ -0,0 +1,5 @@
+from choose_sequences import sequences
+sequences("xenonbaseline.csv",False,5,1,150,False,True)
+sequences("Argon.csv", "xenonbaseline.csv",5,0,150,False,True)
+sequences("gemisch.csv", "xenonbaseline.csv",5,0,150,False,True)
+sequences("Xenon.csv", "xenonbaseline.csv",5,0,150,False,True)
