d7cec6509958858024d70638d7b2747068460201
hacknus <l_stoeckli@bluewin.ch>
2020-11-18 12:01:22 +0100

    gauss fit

--- air.py
+++ air.py
@@ -1,9 +1,15 @@
 from choose_sequences import sequences
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+def gauss(m, a, mu, sigma):
+    return a * np.exp(- (m - mu) ** 2 / (2 * sigma ** 2))
+
 
 relative = False
-combine = 5
+combine = 1
 start = 0
 end = 50
 
@@ -15,9 +21,23 @@ o2[0] = p0[32 - 1]
 co2[0] = p0[44 - 1]
 for i in np.arange(1, 8):
     amu, p, err = sequences("air{}.csv".format(i), False, combine, start, end, relative, False)
-    p = p * p0[int(5 * 28 / combine - 1)] / p[int(5 * 28 / combine - 1)]
-    o2[i] = p[32 - 1]
-    co2[i] = p[44 - 1]
+    mask_o2 = (amu > 31) & (amu < 33)
+    mask_co2 = (amu > 43) & (amu < 45)
+    popt, pcov = curve_fit(gauss, amu[mask_o2], p[mask_o2], p0=[max(p[mask_o2]), 32, 1])
+    o2[i] = popt[0]
+    o2popt = popt
+    popt, pcov = curve_fit(gauss, amu[mask_co2], p[mask_co2], p0=[max(p[mask_co2]), 44, 1])
+    co2[i] = popt[0]
+    print(popt[0])
+    plt.plot(amu[mask_co2], p[mask_co2], color="orange")
+    plt.plot(np.linspace(43, 45, 100), gauss(np.linspace(43, 45, 100), *popt), ls="--", color="red")
+    plt.plot(amu[mask_o2], p[mask_o2], color="blue")
+    plt.plot(np.linspace(31, 33, 100), gauss(np.linspace(31, 33, 100), *o2popt), ls="--", color="red")
+    plt.show()
+    # exit()
+    # p = p * p0[int(5 * 28 / combine - 1)] / p[int(5 * 28 / combine - 1)]
+    # o2[i] = p[32 - 1]
+    # co2[i] = p[44 - 1]
 # plt.plot(amu,p,label='breathing {}'.format(i))
 # plt.errorbar(amu, p, err, capsize=3, capthick=0.4 ,ecolor="black", elinewidth=0.4 ,fmt ='none')
 #
