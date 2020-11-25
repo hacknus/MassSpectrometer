d9207d575c0014b5f043f572bac72ff182112025
donathess <62433205+donathess@users.noreply.github.com>
2020-11-25 23:24:08 +0100

    Update all_gases.py

--- all_gases.py
+++ all_gases.py
@@ -8,6 +8,7 @@ from utils import fit_peak
 from nist import get_nist_peaks, nist_aprox
 from gas_analysis import gas_analysis
 
+<<<<<<< HEAD
 
 
 
@@ -18,3 +19,12 @@ nist_aprox(*gas_analysis('mix2.csv','mix_baseline2.csv'),'mix')
 nist_aprox(*gas_analysis('co2_premature_balloon_loss.csv','co2baseline.csv'),r'$CO_2$',True)
 nist_aprox(*gas_analysis('deo.csv','airbaseline.csv'),'Deo')
 nist_aprox(*gas_analysis('ethanol.csv','co2baseline.csv'),'Ethanol',True)
+=======
+nist_aprox(*gas_analysis('xenon_highres.csv', 'xenonbaseline_highres.csv'), 'xenon')
+nist_aprox(*gas_analysis('xenonbaseline_highres.csv', False, ), 'resiudal gas')
+nist_aprox(*gas_analysis('argon2.csv', 'argonbaseline.csv'), 'argon')
+nist_aprox(*gas_analysis('mix2.csv', 'mix_baseline2.csv'), 'mix')
+nist_aprox(*gas_analysis('co2_premature_balloon_loss.csv', 'co2baseline.csv'), r'$CO_2$')
+nist_aprox(*gas_analysis('deo.csv', 'airbaseline.csv'), 'Deo')
+nist_aprox(*gas_analysis('ethanol.csv', 'co2baseline.csv'), 'Ethanol', True)
+>>>>>>> 202ac3ab2ceb119e8e6fc80624c1a0e57aaed4bc
