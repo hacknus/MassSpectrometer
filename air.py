a6bd4029dec0b059e58c8d8dee28f63a680ae0fe
hacknus <l_stoeckli@bluewin.ch>
2020-11-30 00:05:17 +0100

    updated air analysis

--- air.py
+++ air.py
@@ -11,7 +11,7 @@ def oxygen(n, n0, p):
 
 
 def carbondioxide(n, n0, p):
-    return n0 - n0 * p ** n
+    return n0 * (1 - p ** n)
 
 
 relative = False
@@ -41,7 +41,7 @@ for i, c in zip(breath, colors):
     co2.append(np.sqrt(2 * np.pi) * popt[0] * popt[2])
     co2err.append(2 * np.pi * np.sqrt(popt[0] ** 2 * pcov[2][2] + popt[2] ** 2 * pcov[0][0]))
 
-    ax.bar(amu, p, color=c, width=0.1, edgecolor="black", label="air measurement", alpha=0.5)
+    ax.bar(amu, p, color=c, width=0.1, edgecolor="black", label=f"air measurement {i}", alpha=0.5)
     ax.errorbar(amu, p, err, capsize=3, capthick=0.4, ecolor="black", elinewidth=0.4, fmt='none')
     s = np.sum(p)
     co2[-1] /= s
@@ -50,25 +50,25 @@ for i, c in zip(breath, colors):
     o2err[-1] /= s
     # plt.xlim(30, 50)
     # plt.semilogy()
-    plt.ylim(1e-9, 3e-7)
-    plt.xlim(41.5, 46.5)
-    plt.xticks(range(42, 47))
-    plt.legend()
-    plt.ylabel(r"$p$ [Torr]")
-    plt.xlabel(r"$m$ [amu]")
-    plt.savefig("Report/DataResultsPlots/peak.pdf")
-    plt.show()
-    exit()
+plt.ylim(1e-9, 3e-7)
+plt.xlim(41.5, 46.5)
+plt.xticks(range(42, 47))
+plt.legend()
+plt.ylabel(r"$p$ [Torr]")
+plt.xlabel(r"$m$ [amu]")
+plt.savefig("Report/DataResultsPlots/peak.pdf")
+plt.show()
+# exit()
 
 popt, pcov = curve_fit(oxygen, np.arange(1, 6), o2, sigma=o2err)
 n = np.linspace(0, 6, 100)
-print(f"a = {100 * popt[1]:.4f} +/- {100 * np.sqrt(pcov[1][1]):.4f}")
+print(f"a = {popt[1]:.4f} +/- {np.sqrt(pcov[1][1]):.4f}")
 print(f"n0 = {100 * popt[0]:.4f} +/- {100 * np.sqrt(pcov[0][0]):.4f}")
 plt.plot(n, 100 * oxygen(n, *popt), ls="--", color="red")
 
 popt, pcov = curve_fit(carbondioxide, np.arange(1, 6), co2, sigma=co2err, p0=popt)
 n = np.linspace(0, 6, 100)
-print(f"a = {100 * popt[1]:.4f} +/- {100 * np.sqrt(pcov[1][1]):.4f}")
+print(f"a = {popt[1]:.4f} +/- {np.sqrt(pcov[1][1]):.4f}")
 print(f"n0 = {100 * popt[0]:.4f} +/- {100 * np.sqrt(pcov[0][0]):.4f}")
 plt.plot(n, 100 * carbondioxide(n, *popt), ls="--", color="blue")
 
--- ethanol.py
+++ ethanol.py
@@ -9,16 +9,28 @@ combine = 1
 start = 0
 end = 50
 
-n = get_nist_peaks("ethanol", p_number=3)
+n = get_nist_peaks("ethanol", p_number=10)
+nn = get_nist_peaks("nitrogen", p_number=4)
+nco2 = get_nist_peaks("co2", p_number=4)
+no2 = get_nist_peaks("oxygen", p_number=4)
+nag = get_nist_peaks("argon", p_number=4)
 
 amu, p, err = sequences('ethanol.csv', False, combine, start, end, relative, False, new=True)
 fig, ax = plt.subplots(1, 1)
 
 scale = np.max(p[(amu > 30) & (amu < 32)]) / 10000
+scalen = np.max(p[(amu > 27) & (amu < 29)]) / 10000
+scaleco2 = np.max(p[(amu > 43) & (amu < 45)]) / 10000
+scaleo2 = np.max(p[(amu > 31.5) & (amu < 33)]) / 10000
+scaleag = np.max(p[(amu > 39) & (amu < 41)]) / 10000
 
-ax.bar(n.m, n.y * scale, width=0.9, alpha=0.5, color="orange", label="NIST Ethanol")
+ax.bar(n.m, n.y * scale, width=0.9, alpha=0.5, color="red", label="NIST Ethanol")
+ax.bar(nco2.m, nco2.y * scaleco2, width=0.9, alpha=0.5, color="orange", label=r"NIST CO$_2$")
+ax.bar(nn.m, nn.y * scalen, width=0.9, alpha=0.5, color="green", label="NIST N$_2$")
+ax.bar(no2.m, no2.y * scaleo2, width=0.9, alpha=0.5, color="yellow", label="NIST O$_2$")
+ax.bar(nag.m, nag.y * scaleag, width=0.9, alpha=0.5, color="brown", label="NIST Ag")
 
-ax.bar(amu, p, width=0.1 * combine, color="red", label="ethanol")
+ax.bar(amu, p, width=0.1 * combine, color="blue", label="ethanol + air")
 ax.errorbar(amu, p, err, capsize=3, capthick=0.4, ecolor="black", elinewidth=0.4, fmt='none')
 # popt, pcov = fit_peak(amu, p, m1=start, m2=end, ax=ax)
 ax.legend()
@@ -28,6 +40,7 @@ else:
     ax.set_ylabel(r"$p$ [Torr]")
 ax.set_xlabel("amu")
 ax.set_ylim(0, 1e-6)
-ax.set_xlim(25, 50)
+ax.set_xlim(25, 47)
+ax.set_xticks(range(25, 47))
 plt.savefig("Report/DataResultsPlots/ethanol2.pdf")
 plt.show()
