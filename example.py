a8d3fe517e5aeffefe5f152ea3161a618847dce6
hacknus <l_stoeckli@bluewin.ch>
2020-12-28 13:03:02 +0100

    PEP-8 format

--- example.py
+++ example.py
@@ -1,10 +1,6 @@
 from nist import nist_approx
 from gas_analysis import gas_analysis
 
-
-
-
-
 if __name__ == "__main__":
     params = gas_analysis('deo.csv', 'airbaseline.csv')
-    air = nist_approx(*params, 'deo')
+    deo = nist_approx(*params, 'deo')
